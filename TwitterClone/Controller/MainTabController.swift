8653cd059629269dbf3345322f3cea80b5ea203f
Roman Tsvetkov <tsvetkovv22@gmail.com>
2020-05-17 14:09:17 +0300

    feat: configure the project

---
+++ TwitterClone/Controller/MainTabController.swift
@@ -0,0 +1,13 @@
+//
+// Created by Роман Цветков on 17.05.2020.
+// Copyright (c) 2020 Roman Tsvetkov. All rights reserved.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+}
