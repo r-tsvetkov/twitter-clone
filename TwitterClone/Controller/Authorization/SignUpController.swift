7733a2071def67b2f41e224ec67fd50887c74ba1
Roman Tsvetkov <tsvetkovv22@gmail.com>
2020-05-20 09:13:36 +0300

    feat: finished the login and signup screens

--- TwitterClone/Controller/Authorization/SignUpController.swift
+++ TwitterClone/Controller/Authorization/SignUpController.swift
@@ -8,6 +8,8 @@ import UIKit
 class SignUpController: UIViewController {
 
     // MARK: - Properties
+    private let imagePicker = UIImagePickerController()
+
     private let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(named: "plus_photo"), for: .normal)
@@ -102,12 +104,15 @@ class SignUpController: UIViewController {
     }
     
     @objc func handleAddProfilePhoto() {
-
+        present(imagePicker, animated: true)
     }
 
     // MARK: - Helpers
     func configureUI() {
         view.backgroundColor = .twitterBlue
+
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
 
         view.addSubview(plusPhotoButton)
         plusPhotoButton.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
@@ -140,5 +145,23 @@ class SignUpController: UIViewController {
                 paddingLeft: 40,
                 paddingRight: 40
         )
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension SignUpController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    public func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        guard let profileImage = info[.editedImage] as? UIImage else { return }
+
+        plusPhotoButton.layer.cornerRadius = 128 / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.imageView?.contentMode = .scaleAspectFill
+        plusPhotoButton.imageView?.clipsToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.white.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+        plusPhotoButton.setImage(profileImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        dismiss(animated: true)
     }
 }
